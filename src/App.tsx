5dadcf7c6354ecc818741093df7a7dcaa65448b7
Казымов Чингиз Теймур <Kazy_mv19@itstep.edu.az>
2023-10-02 12:11:01 +0400

    first steps to create react app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Paragraph from './Components/Paragraph/Paragraph'
+function App(){
+    return(
+        <Paragraph text='WoooHooo'/>
+    )
+}
+export default App;
