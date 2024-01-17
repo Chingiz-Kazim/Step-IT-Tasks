a2f005f885af9cc5907719e7c72ed1377311ac5b
Chingiz-Kazim <kazimov.chingiz.111@gmail.com>
2024-01-17 20:39:54 +0400

    added new project

--- Rooting_React/src/routes/destroy.jsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "react-router-dom";
-import { deleteTask } from "../tasks";
-
-export async function action({ params }) {
-    //throw new Error("oh dang!");
-    await deleteTask(params.taskId);
-    return redirect("/");
-}
